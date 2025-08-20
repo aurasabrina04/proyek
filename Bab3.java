6846e3ec9178fc817918ceb2d37a7e670273cb3a
aurasabrina04 <aurasabriana862@gmail.com>
2025-08-20 10:00:46 +0700

    Add files via upload

---
+++ Bab3.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author PC 31 - LAB R1
+ */
+public class Bab3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+
+) // TODO code application logic here
+//        System.out.println("halo"+""+"dunia");
+//        System.out.println(2+""+5);
+//        System.out.println(2 + 5);
+//       System.out.println("Angka " + " " + 5);
+//      System.out.println("2 + 5" + " = " + 2 + 5);
+//       System.out.println("2 + 5" + " = " + (2 + 5));
+//        String namaDepan = "";
+//        String namaBelakang = "";
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nama Depan : ");
+////membaca inputan user
+//        namaDepan = input.next();
+//        System.out.println("Nama Belakang : ");
+////membaca inputan user
+//        namaBelakang = input.next();
+//        System.out.println("Nama Saya adalah :");
+//        System.out.println(namaDepan + " " + namaBelakang);
+//String namaDepan = "";
+//        String namaBelakang = "";
+////buat objek dari inputStream
+//        InputStreamReader ireader
+//                = new InputStreamReader(System.in);
+////buat objek bufferreader
+//        BufferedReader breader
+//                = new BufferedReader(ireader);
+//        System.out.println("Nama Depan : ");
+////membaca inputan user
+//        namaDepan
+//                = breader.readLine();
+//        System.out.println("Nama Belakang : ");
+////membaca inputan user
+//        namaBelakang
+//                = breader.readLine();
+//        System.out.println("Nama Saya adalah :");
+//        System.out.println(namaDepan + " "
+//                + namaBelakang);
+//
+//String a=
+//JOptionPane.showInputDialog("Masukan nilai untuk variabel a: ");
+//
+//String b =
+//
+//JOptionPane.showInputDialog("Masukan nilai untuk variabel b: ");
+//
+//System.out.println("Hasil :"+
+//        
+//        (Integer.parseInt(a)*
+//         Integer.parseInt(b)));
+//String nama = "Budi";
+//
+//String jurusan = "Sistem Informasi";
+//
+//JOptionPane.showMessageDialog(null, "Halo 10" + nama+ ", jurusan Kamu" + jurusan);
+    
+JOptionPane.showMessageDialog(
+null, "Sebuah Informasi.", "Informasi",
+JOptionPane.INFORMATION_MESSAGE);
+
+
+  JOptionPane.showMessageDialog( null, "Sebuah Error!", "Error", JOptionPane.ERROR_MESSAGE);
+
+}  
+    }  
+    
+   
+    
+
+
+
+    
+
